40018721270f332801a2436e3d11a6f07f4926ca
Isaac Hernandez <isdhdezc97@gmail.com>
2022-01-18 10:03:54 +0000

    feature react-boostrap added

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 //import your own components
 import Layout from "./layout";
 
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,16 +2,20 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Card from "../component/Card.jsx"
+
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<Card/>
+			{/* <h1>Hello Rigo!!</h1>
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
+			
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
@@ -20,7 +24,7 @@ export const Home = () => {
 				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
 					Read documentation
 				</a>
-			</p>
+			</p> */}
 		</div>
 	);
 };
