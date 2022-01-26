ded36511f40531f7307c293d97c20ce3126737d8
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-01-26 20:00:34 +0100

    Initial RAJ models

--- src/api/backend/models/ORMobjects.py
+++ src/api/backend/models/ORMobjects.py
@@ -1,48 +1,54 @@
+from email.policy import default
 from backend.models import db
 
-aux_planets = db.Table('planets',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('planet_id', db.Integer, db.ForeignKey('planet.id'), primary_key=True)
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    phone = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(250), nullable=False, unique=True)
+    password_hash = db.Column(db.String(250), nullable=False)
+    status = db.Column(db.Boolean, nullable=False, default=False)
+    role = db.Column(db.String(3), nullable=False)
+    companies = db.relationship('Company', secondary="account_company_relationship", lazy='subquery',
+        backref = db.backref('accounts', lazy=True))
+    def __repr__(self):
+        return '<Account id: %r - %s>' % (self.id, self.username)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "phone": self.phone,
+            "username": self.username,
+        }
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    NIF = db.Column(db.String(15), nullable=False)
+    address = db.Column(db.String(256))
+    def __repr__(self):
+        return '<Company id: %r - %s>' % (self.id, self.name)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "NIF": self.NIF,
+            "address": self.address
+        }
+
+class Roll(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(50), nullable=False)
+    def __repr__(self):
+        return '<Roll id: %r - %s>' % (self.id, self.description)
+    def serialize(self):
+        return { "id": self.id, "description": self.description }
+
+account_company_relationship = db.Table('account_company_relationship',
+    db.Column('account_id', db.Integer, db.ForeignKey(Account.id), primary_key=True),
+    db.Column('company_id', db.Integer, db.ForeignKey(Company.id), primary_key=True)
 )
-
-aux_characters = db.Table('characters',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('character_id', db.Integer, db.ForeignKey('character.id'), primary_key=True)
-)
-
-class User(db.Model):
-    __tablename__="user"
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(250), nullable=False, unique=True)
-    password_hash=db.Column(db.String(250))
-    planets = db.relationship('Planet', secondary=aux_planets, lazy='subquery',
-        backref=db.backref('users', lazy=True))
-    characters = db.relationship('Character', secondary=aux_characters, lazy='subquery',
-        backref=db.backref('users', lazy=True))
-    def __repr__(self):
-        return '<User id: %r - %s>' % (self.id, self.username)
-    def serialize(self):
-        return { "id": self.id, "username": self.username }
-
-class Character(db.Model):
-    __tablename__="character"
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(250), nullable=False, unique=True)
-    description=db.Column(db.String(512), nullable=False)
-    img_path=db.Column(db.String(256))
-    def __repr__(self):
-        return '<Character id: %r - %s>' % (self.id, self.name)
-    def serialize(self):
-        return { "id": self.id, "name": self.name, "description": self.description }
-
-class Planet(db.Model):
-    __tablename__="planet"
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(250), nullable=False, unique=True)
-    description=db.Column(db.String(512), nullable=False)
-    img_path=db.Column(db.String(256))
-    def __repr__(self):
-        return '<Planet id: %r - %s>' % (self.id, self.name)
-    def serialize(self):
-        return { "id": self.id, "name": self.name, "description": self.description, "img_path": self.img_path }
-
--- src/api/backend/models/__init__.py
+++ src/api/backend/models/__init__.py
@@ -3,3 +3,4 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from backend.models.ORMobjects import *
+from backend.models.generics import *
