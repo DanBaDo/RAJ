ea0f645337af00bce008115473652eb8cbf5584c
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-01-26 20:01:19 +0100

    Fits access control to RAJ model

--- src/api/backend/access_control.py
+++ src/api/backend/access_control.py
@@ -1,16 +1,16 @@
 from os import environ
 from flask_jwt_extended import JWTManager
 from backend import app
-from backend.models import User
+from backend.models import Account
 
 app.config["JWT_SECRET_KEY"] = environ.get("JWT_SECRET_KEY")
 jwt = JWTManager(app)
 
 @jwt.user_identity_loader
-def user_identity_lookup(user):
-    return user.id
+def user_identity_lookup(account):
+    return account.id
 
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
-    return User.query.filter_by(id=identity).one_or_none()
+    return Account.query.filter_by(id=identity).one_or_none()
