94a97609695fa68db4becf5a1cf1fd3de21be426
DanBaDo <daniel.banobre.dopico@protonmail.com>
2022-01-31 20:22:28 +0100

    Adds network layer test and component tests

--- src/front/js/component/NavbarSection/NavbarTop.jsx
+++ src/front/js/component/NavbarSection/NavbarTop.jsx
@@ -12,6 +12,8 @@ const NavbarTop = () => {
           <Nav className="me-auto">
             <Link to="/">Home</Link>
             <Link to="/FormAffected">Formulario Usuario</Link>
+            <Link to="/tests/">Component tests</Link>
+            <Link to="/networkLayer/">Network Layer</Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,8 @@ import { Home } from "./pages/home";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 import {FormAffected} from "./pages/FormAffected";
+import { Tests } from "./pages/Tests";
+import { NetworkLayer } from "./pages/NetworkLayerTest";
 
 const Layout = () => {
 	const basename = process.env.BASENAME || "";
@@ -20,9 +22,16 @@ const Layout = () => {
 						<Route exact path="/single/:theid">
 							<Single />
 						</Route>
+						<Route exact path="/tests/">
+							<Tests />
+						</Route>
+						<Route exact path="/networkLayer/">
+							<NetworkLayer />
+						</Route>
 						<Route exact path="/">
 							<Home />
 						</Route>
+
 						<Route>
 							<h1>Not found!</h1>
 						</Route>
