3b1f565c746af5cd2a53c5c4ea2aa9b848afb1c8
DanBaDo <daniel.banobre.dopico@protonmail.com>
2022-01-31 17:04:01 +0100

    Minor cleanup

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-import {FormAffected} from "./pages/FormAffected.js";
+import {FormAffected} from "./pages/FormAffected";
 
 const Layout = () => {
 	const basename = process.env.BASENAME || "";
