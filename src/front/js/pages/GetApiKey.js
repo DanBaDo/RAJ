edc2d45b22d4104065d010f8711fc7ecf6b34cb6
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2022-02-06 20:03:46 +0100

    Encloses network request in useEffect. Show all errors through main error handler

--- src/front/js/pages/GetApiKey.js
+++ src/front/js/pages/GetApiKey.js
@@ -1,29 +1,43 @@
-import React, {useState} from "react";
+import React, {useEffect, useState, useContext} from "react";
+import { Context } from "../store/appContext";
 import styled from "styled-components";
 import { getAPIKeys } from "../libraries/request/APIRequests";
 import { Container, Row, Col } from "react-bootstrap";
 import { FaRegCopy, FaQrcode, FaTrash } from "react-icons/fa"
 
 const GetApiKey = () => {
+  const { store, actions } = useContext(Context);
   const [elements, setElements] = useState([]);
-  getAPIKeys.onErrror = (error)=>console.error(error);
-  getAPIKeys.onResponse = (response) => {
-    switch (response.code) {
-      case 200:
-        const elem = response.contents.data.map(
-          (key) => <li key={key.id}>API key {key.id} <FaRegCopy/><FaQrcode/><FaTrash/></li>
-        );
-        setElements(elem);
-        break;
-      case 403:
-        console.error('Autentication error getting API keys list');
-        break;
-      default:
-        console.error('Unexpected error getting API keys list');
-        break;
-    };
-  };
-  getAPIKeys.call()
+  useEffect(
+    ()=>{
+      getAPIKeys.onError = (error) => {
+        actions.addError(error);
+      }
+      getAPIKeys.onResponse = (response) => {
+        try {
+          switch (response.code) {
+            case 200:
+              const elem = response.contents.data.map(
+                (key) => <li key={key.id}>API key {key.id} <FaRegCopy/><FaQrcode/><FaTrash/></li>
+              );
+              setElements(elem);
+              break;
+            case 403:
+              throw "Autentication error getting API keys list";
+              break;
+            default:
+              throw "Unexpected error getting API keys list";
+              break;
+          };
+        } catch (error) {
+          actions.addError(error);
+        }
+      };
+      getAPIKeys.call()
+    },
+    []
+  );
+
 
   return (
     <>
