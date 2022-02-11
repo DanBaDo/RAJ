5ecfaf4074ae0a98f09806b77d04fef3adf68114
adriangualsaqui <adriangualsaqui@hotmail.com>
2022-02-11 20:38:33 +0000

    img lodupatia

--- src/front/js/component/HelpSection/HelpSection.jsx
+++ src/front/js/component/HelpSection/HelpSection.jsx
@@ -11,14 +11,13 @@ const HelpSection = () => {
         <Container className="my-3">
             <Row>
                 <Col md={{order:"first"}} xs={{ order: 'last' }}>
-                    <ContainerImg src="https://image.freepik.com/foto-gratis/primer-plano-mano-ofrecimiento-hombre-negocios-apreton-manos_1262-17295.jpg?w=740" alt="primer-plano-mano-ofrecimiento-hombre-negocios-apreton-manos" />
+                    <ContainerImg src="https://okdiario.com/img/2020/10/19/tratamiento-de-la-ludopatia-en-los-mas-jovenes.jpg" alt="primer-plano-mano-ofrecimiento-hombre-negocios-apreton-manos" />
                 </Col>
                 <Col md={6} xs={12}>
-                    <h3>Permitenos Ayudarte</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint repellat 
-                        recusandae iure, earum quos quam cupiditate doloribus quis, 
-                        veniam vel soluta eos aut ipsa ipsam, modi perspiciatis facere optio. 
-                        Saepe.</p>       
+                    <h1>¿Que es la ludopatia?</h1>
+                    <p>La ludopatía es la adicción a los juegos de azar. Esto puede llevar a la pérdida de trabajo, problemas de dinero, fraude, crimen o problemas en las relaciones familiares.
+
+</p>       
                 </Col>
             </Row>
         </Container>
