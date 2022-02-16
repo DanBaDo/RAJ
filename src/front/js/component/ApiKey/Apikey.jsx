a14e12451aea15265a8dad36acacf9c9b91cb25b
adriangualsaqui <adriangualsaqui@hotmail.com>
2022-02-16 19:58:24 +0000

    edit apikey

--- src/front/js/component/ApiKey/Apikey.jsx
+++ src/front/js/component/ApiKey/Apikey.jsx
@@ -2,17 +2,24 @@
 import React, { useEffect, useState, useContext } from "react";
 import { FaRegCopy, FaQrcode, FaTrash } from "react-icons/fa";
 import BigQR from "../Commoncomponents/bigQR.jsx";
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Col, Row, CloseButton, } from 'react-bootstrap';
 
 const ApiKey = (props) => {
     const [QRData, setQRData] = useState(null);
+    const handleClose = () => setShow(false);
     return (
         <ListGroup>
             <ListGroup.Item>
-                API key {props.id}
-                <FaRegCopy />
-                <FaQrcode onClick={() => { setQRData(props.url) }} />
-                <FaTrash />
+                <Row>
+                    <Col xs={4} md={8} >{props.id}.Dispositivo activo</Col>
+                    <Col xs={2} md={1}><FaRegCopy /></Col>
+                    <Col xs={2} md={1}><FaQrcode onClick={() => { setQRData(props.url) }} /></Col>
+                    <Col xs={2} md={1}><FaTrash /></Col>
+                    <Col xs={2} md={1}><CloseButton onClick={handleClose} /></Col>
+                </Row>
+
+
+
             </ListGroup.Item>
             {QRData && <BigQR url={QRData} close={() => { setQRData(null) }} />}
         </ListGroup>
