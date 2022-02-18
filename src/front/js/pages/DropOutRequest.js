1126ad070110d2b21a631618b081441a28ace7ee
adriangualsaqui <adriangualsaqui@hotmail.com>
2022-02-18 23:25:43 +0000

    style button

--- src/front/js/pages/DropOutRequest.js
+++ src/front/js/pages/DropOutRequest.js
@@ -1,23 +1,50 @@
 import React from "react";
-import { Button,Container } from "react-bootstrap";
+import { Button, Col, Container, Form, Check } from "react-bootstrap";
 
 const DropOutRequest = () => {
   return (
     <>
-    <Container className="text-center mt-5">
-    <h1>Baja </h1>
-      <p>
-        Estas apunto de  darte de baja!
-        <p><span>Darse de baja de <strong>RAJ</strong> no es tan facil. </span></p>
-        Nuestro equipo se pondra en contacto contigo despues de <strong>72h</strong>.
+      <Container className="text-center mt-5">
+        <h1>Baja </h1>
         <p>
-          La Ludopatia no se cura , solo se trata y por eso queremos estar seguros de que tomas la decisión correcta.
-
-
+          Estas apunto de darte de baja!
+          <p>
+            <span>
+              Darse de baja de <strong>RAJ</strong> no es tan facil.{" "}
+            </span>
+          </p>
+          Nuestro equipo se pondra en contacto contigo despues de{" "}
+          <strong>72h</strong>.
+          <p>
+            La Ludopatia no se cura , solo se trata y por eso queremos estar
+            seguros de que tomas la decisión correcta.
+          </p>
+          <span>Estas seguro que quieres darte de baja </span>
+          <Col class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio1"
+              value="option1"
+            ></input>
+            <label class="form-check-label" for="inlineRadio1">
+              Si
+            </label>&nbsp;&nbsp;&nbsp;
+            <input
+              class="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio2"
+              value="option2"
+            ></input>
+            <label class="form-check-label" for="inlineRadio2">
+              NO
+            </label>
+          </Col>
         </p>
-      </p>
-    <Button className="button">Darse de baja</Button>
-    </Container>
+        <Button className="button">Darme de baja</Button>
+      </Container>
     </>
   );
 };
