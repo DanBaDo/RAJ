3a91bf9d98804e421685e7b95e0aa96ea08a9a2a
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-02-24 22:59:04 +0100

    Connect GetApiKeys with backend

--- src/front/js/pages/GetApiKey.js
+++ src/front/js/pages/GetApiKey.js
@@ -3,7 +3,7 @@ import { Context } from "../store/appContext";
 import styled from "styled-components";
 import { getAPIKeys } from "../libraries/request/APIRequests";
 import { Container, Row, Col, Button, Modal, Form } from "react-bootstrap";
-
+import { createAPIKeys } from "../libraries/request/APIRequests";
 import Apikey from "../component/ApiKey/Apikey.jsx";
 import "./GetApiKey.scss";
 
@@ -12,17 +12,19 @@ const GetApiKey = () => {
   const [keys, setKeys] = useState([]);
   const [QRData, setQRData] = useState(null);
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  //New API key modal
+  const [formData, setFormData] = useState({"purpose": "APPLICATION"});
+  const [validated, setValidated] = useState(false);
+
 
   const keysComponents = () => {
     return keys.map((key, idx) => (
-      <Apikey id={key.id} url={key.url} key={idx}></Apikey>
+      <Apikey description={key.description} url={key.url} key={idx} installed={key.installed}></Apikey>
     ));
   };
 
   useEffect(() => {
-    /*getAPIKeys.onError = (error) => {
+    getAPIKeys.onError = (error) => {
         actions.addError(error);
       }
       getAPIKeys.onResponse = (response) => {
@@ -48,8 +50,8 @@ const GetApiKey = () => {
           actions.addError(error);
         }
       };
-      getAPIKeys.call()*/
-    setKeys([
+      getAPIKeys.call()
+    /*setKeys([
       {
         id: 1,
         key: "8ebfd8977b010655bfdd3c353c234e5e8472b6ac51c1ae1cab3fe06fad053beb",
@@ -65,46 +67,74 @@ const GetApiKey = () => {
         key: "b6ac51c1ae1cab3fe06fad053beb8ebfd853c234e5e8472977b010655bfdd3c3",
         url: "/apikey/b6ac51c1ae1cab3fe06fad053beb8ebfd853c234e5e8472977b010655bfdd3c3",
       },
-    ]);
+    ]);*/
   }, []);
+
+  const handleClose = () => setShow(false);
+
+  const handleShow = () => setShow(true);
+
+  const handleChange = (event) => {
+    const currentFormData = {...formData};
+    currentFormData[event.target.name] = event.target.value;
+    setFormData(currentFormData);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+    setValidated(true);
+    createAPIKeys.onError = (error) => actions.addError(error);
+    createAPIKeys.onResponse = (resp)=>{
+      console.log(resp)
+      handleClose()
+    }
+    createAPIKeys.data = formData;
+    createAPIKeys.call();
+  };  
 
   return (
     <>
       <Container>
+
         <Row className="justify-content-center">
           <Col>
-            <Col>
+              <h1 className="TitleApi">Claves API</h1>
+              <ul>{keysComponents()}</ul>
               <Button variant="primary" onClick={handleShow}>
                 Añadir
               </Button>
-            </Col>
-            <Col>
-              <h1 className="TitleApi">Dispositivos Permitidos</h1>
-              <ul>{keysComponents()}</ul>
-            </Col>
           </Col>
         </Row>
 
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Añadir Dispositivo</Modal.Title>
+            <Modal.Title>Nueva clave API</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Form>
+            <Form  onChange={handleChange}  noValidate validated={validated}  onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
+                <Form.Label>Nombre descriptivo</Form.Label>
+                <Form.Control type="text" name="description" placeholder="Diferencie sus claves API" />
               </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Uso de la llave</Form.Label>
+                <Form.Check 
+                  type="radio"
+                  name="purpose"
+                  label="Servicio on-line"
+                  value="APPLICATION"
+                  defaultChecked
+                />
+                <Form.Check 
+                  type="radio"
+                  name="purpose"
+                  label="Lector de control de acceso"
+                  value="IOT"
+                />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Submit
@@ -113,10 +143,7 @@ const GetApiKey = () => {
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
+              Cancelar
             </Button>
           </Modal.Footer>
         </Modal>
