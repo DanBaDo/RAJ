84776bc634023fb44f20f566ada16845cb1114bf
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-02-24 22:20:55 +0100

    Fits API_key model

--- src/api/backend/models/database.py
+++ src/api/backend/models/database.py
@@ -57,7 +57,8 @@ class API_key(db.Model):
     key = db.Column(db.String(262), primary_key=True)
     description = db.Column(db.String(100), nullable=False)
     installed = db.Column(db.String(5), nullable=False, default=False)
-    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    purpose = db.Column(db.Integer, nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), default=0 ,nullable=False)
     def __repr__(self):
         return '<API key : %s >' % (self.description)
     def serialize(self):
