012e74b1892c3ac1a54f7b5520fba7fb7822dc8c
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-02-24 06:38:04 +0100

    Fixes merge conflict

--- src/api/backend/endpoints/__init__.py
+++ src/api/backend/endpoints/__init__.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-from backend.endpoints import accounts_endpoint
-=======
 from backend.endpoints import public_api_endpoints
 from backend.endpoints import accounts_endpoints
->>>>>>> feature/api
