9d5f2c524ee59440cc9acb30fc445bac06cfcfb4
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-03-12 19:33:28 +0100

    Fixes problems importing SVG files

--- webpack.common.js
+++ webpack.common.js
@@ -13,26 +13,28 @@ module.exports = {
     publicPath: '/'
   },
   module: {
-    rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        },
-        {
-          test: /\.(css|scss)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        {
-          test: /\.(png|svg|jpg|gif|jpeg|webp)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' }
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
+    rules: [ 
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/, use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }]
+      }, //css only files
+      {
+        test: /\.(png|svg|jpg|gif|jpeg|webp)$/, use: {
+          loader: 'file-loader',
+          options: { name: '[name].[ext]' }
+        }
+      }, //for images
+      { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)/, use: ['file-loader'] } //for fonts
+      // { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
+
     ]
   },
   resolve: {
