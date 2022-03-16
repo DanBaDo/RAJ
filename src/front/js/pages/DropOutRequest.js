3da9bba776f116d0f0fccd4bb8a9a6019abbd2d1
bigbitdev <git config --global user.email dangdev97@gmail.com>
2022-03-16 14:05:06 +0100

    dropout request page fixed

--- src/front/js/pages/DropOutRequest.js
+++ src/front/js/pages/DropOutRequest.js
@@ -5,25 +5,21 @@ const DropOutRequest = () => {
   return (
     <>
       {" "}
-        <Container>
-          <Row>
-          <Col>
-            <h1>Baja </h1>
-            Estas apunto de darte de baja!
+      <style>{"body{background-color:#1f2b5b}"}</style>
+      <Container className="bg-white rounded py-3 my-4">
+        <Row>
+          <Col className="text-center">
+            <h1>Solicitud de Baja de RAJ</h1>
             <p>
-              <span>
-                Darse de baja de <strong>RAJ</strong> no es tan facil.{" "}
-              </span>
+              Estas apunto de darte de baja! Aunque darse de baja de  
+              RAJ no es tan facil.<br/> 
+              Nuestro equipo se pondra en
+              contacto contigo despues de <strong>72h</strong>.
+              <br/> La Ludopatia no
+              se cura , solo se trata y por eso queremos estar seguros de que
+              tomas la decisión correcta.
             </p>
-            Nuestro equipo se pondra en contacto contigo despues de{" "}
-            <strong>72h</strong>.
-            <p>
-              La Ludopatia no se cura , solo se trata y por eso queremos estar
-              seguros de que tomas la decisión correcta.
-            </p>
-            <span>Estas seguro que quieres darte de baja </span>
-          </Col>
-          <Col className="form-check form-check-inline">
+            <span className="pl-1">¿Estas seguro que quieres darte de baja?</span>
             <input
               className="form-check-input"
               type="radio"
@@ -31,9 +27,7 @@ const DropOutRequest = () => {
               id="inlineRadio1"
               value="option1"
             ></input>
-            <label className="form-check-label">
-              Si
-            </label>
+            <label className="form-check-label">Si</label>
             &nbsp;&nbsp;&nbsp;
             <input
               className="form-check-input"
@@ -42,15 +36,11 @@ const DropOutRequest = () => {
               id="inlineRadio2"
               value="option2"
             ></input>
-            <label className="form-check-label">
-              NO
-            </label>
+            <label className="form-check-label">NO</label>
+            <Button className="buttonlogin">Darme de baja</Button>
           </Col>
-          <Col>
-            <Button className="button">Darme de baja</Button>
-          </Col>
-          </Row>
-        </Container>
+        </Row>
+      </Container>
     </>
   );
 };
